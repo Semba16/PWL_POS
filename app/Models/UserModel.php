93ff3a5e84ee92f4bbadff8aa5b30b8f797e17c4
Semba Wijaya <2131710085@student.polinema.ac.id>
2024-06-07 18:27:32 +0700

    Jobsheet 10

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,21 +2,46 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama','password'];
-
-   public function level()
+    protected $fillable = [
+        'level_id',
+        'username',
+        'nama',
+        'password',
+        'image',
+    ];
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    public function level()
     {
-       return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
     }
 }
